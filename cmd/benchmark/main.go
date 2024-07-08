eff58479a1ec6218b2423b13733dfa528da3d973
John Ewart <johnewart@microsoft.com>
2024-07-08 14:08:03 -0700

    Multi-reader benchmark

--- cmd/benchmark/main.go
+++ cmd/benchmark/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
 	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
+
+	"zombiezen.com/go/log"
+)
+
+var (
+	rootDir    = flag.String("root", "", "Root directory to walk")
+	numReaders = flag.Int("readers", 1, "Number of concurrent readers")
 )
 
 func prepareTestDirTree(tree string) (string, error) {
@@ -48,30 +58,86 @@ func readAllFile(path string) (uint64, error) {
 }
 
 func main() {
-	root := "/Users/johnewart/Temp/container"
+	flag.Parse()
+
+	if *rootDir == "" {
+		fmt.Println("Please specify a root directory to walk")
+		return
+	}
+
+	root := *rootDir
 	totalBytes := uint64(0)
 	startTime := time.Now()
 	fmt.Println("Listing files in", root)
+	filePaths := make(map[int][]string, 0)
+	index := 0
 	err := filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
 			return err
 		}
-		fmt.Printf("visited file or dir: %q\n", path)
-		if bytes, err := readAllFile(path); err != nil {
-			fmt.Printf("error reading file %q: %v\n", path, err)
-		} else {
-			totalBytes += bytes
+		if info.IsDir() {
+			return nil
 		}
+		index += 1
+		readerIndex := index % *numReaders
+		if m, ok := filePaths[readerIndex]; !ok {
+			filePaths[readerIndex] = []string{path}
+		} else {
+			filePaths[readerIndex] = append(m, path)
+		}
+
 		return nil
 	})
+
 	if err != nil {
 		fmt.Printf("error walking the path %q: %v\n", root, err)
 		return
 	}
 
+	wg := sync.WaitGroup{}
+	totalBytesReaders := make([]uint64, *numReaders)
+
+	ctx := context.Background()
+
+	for i := 0; i < *numReaders; i++ {
+		totalBytesReaders[i] = 0
+		wg.Add(1)
+		go func(readerIndex int) {
+			filesToRead := filePaths[readerIndex]
+			log.Infof(ctx, "Reader %d reading %d files", readerIndex, len(filesToRead))
+			defer wg.Done()
+			for _, path := range filesToRead {
+				if bytes, err := readAllFile(path); err != nil {
+					fmt.Printf("error reading file %q: %v\n", path, err)
+				} else {
+					totalBytesReaders[readerIndex] += bytes
+				}
+			}
+		}(i)
+	}
+
+	/*	fmt.Printf("visited file or dir: %q\n", path)
+			if bytes, err := readAllFile(path); err != nil {
+				fmt.Printf("error reading file %q: %v\n", path, err)
+			} else {
+				totalBytes += bytes
+			}
+			return nil
+		})
+		if err != nil {
+			fmt.Printf("error walking the path %q: %v\n", root, err)
+			return
+		}*/
+
+	wg.Wait()
+	for i, bytes := range totalBytesReaders {
+		fmt.Printf("Reader %d read %d bytes\n", i, bytes)
+		totalBytes += bytes
+	}
 	endTime := time.Now()
 	timeDelta := endTime.Sub(startTime)
+
 	speed := float64(totalBytes) / timeDelta.Seconds()
 	fmt.Printf("Time to walk: %v\n", timeDelta)
 	fmt.Printf("Total bytes: %d\n", totalBytes)
